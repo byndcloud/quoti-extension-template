990e31b565c88323ab2204574bc5a6f0d098b87a
Arthur Lapprand <lapprand@gmail.com>
2022-03-03 12:47:19 -0300

    feat: add extension base

---
+++ src/pages/example/index.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+v-container
+  v-row
+    v-col
+      v-card(outlined)
+        v-card-title {{ cardTitle }}
+        example
+</template>
+
+<script>
+import registerVuexModules from '@/store'
+import { mapFields } from 'vuex-map-fields'
+import example from '../../components/example/example.vue'
+
+export default {
+  props: ["QuotiApi"],
+  components: {
+    example
+  },
+  computed: {
+    ...mapFields('exampleModule', ['cardTitle'])
+  },
+  beforeCreate() {
+    registerVuexModules.call(this)
+  }
+}
+</script>
+
+<style lang="sass">
+</style>
