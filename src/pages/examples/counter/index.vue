55600821ad5e9fa2f447b5f904b63a2d9a74d757
José Jorge <jose.jorge@beyondcompany.com.br>
2025-07-01 14:10:08 -0300

    add: first page example

---
+++ src/pages/examples/counter/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-container
+  p {{ counter }}
+  v-btn(color="primary" @click="increment")
+    | increment
+</template>
+
+<script>
+export default {
+  data() {
+    return { counter: 0 }
+  },
+  methods: {
+    increment() {
+      this.counter += 1
+    }
+  }
+}
+</script>
