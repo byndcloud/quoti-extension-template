8621b6f1dcf6d75e1769a3330970d2d6e3ab3015
José Jorge <jose.jorge@beyondcompany.com.br>
2025-07-01 14:32:28 -0300

    add: new support a format pug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
-  plugins: ['vuetify'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
+  plugins: ['vuetify'],
   rules: {
     'prettier/prettier': 'warn',
     'vuetify/no-deprecated-classes': 'error',
